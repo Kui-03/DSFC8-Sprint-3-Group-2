40c990c2e57987c619b0113254184fa39fbf4774
Kui-Dong Riman <kim.riman.03@gmail.com>
2022-01-18 23:20:58 +0800

    updated

--- st_pages/conclusion.py
+++ st_pages/conclusion.py
@@ -8,10 +8,10 @@ def display():
     st.write("")
     st.subheader("Conclusion")
     st.write("")
-    conclusion = Image.open("Assets\Images\Conclusion.png")
+    conclusion = Image.open("Assets\Images\Conclusion.PNG")
     st.image(conclusion)
     
     st.write("")
     st.subheader("Recommendations")
-    recommendations = Image.open("Assets\Images\Recommendations.png")
+    recommendations = Image.open("Assets\Images\Recommendations.PNG")
     st.image(recommendations)
